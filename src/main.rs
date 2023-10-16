6e9bcc191090e2f85fc65d0c7a9e316e0cf8cce4
mark <mark.slater@mail.com>
2023-10-16 19:56:52 +0100

    Failing six queens (no backtracking).

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod board;
 fn main() {
     let mut board = Board::new();
     let mut place = 0;
-    for queen in 0..5 {
+    for queen in 0..6 {
         if let Ok(t) = board.place_next(place){
             board = t.0;
             place = t.1;
