2937f4fc7f5f13f8d8aa43f6dcca0d17265f2361
mark <mark.slater@mail.com>
2023-10-16 19:44:37 +0100

    Make discovery of all three queens consistent.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ use board::Board;
 mod board;
 
 fn main() {
-    let first = 0;
-    let board_result = Board::new().set(first);
-    if let Ok(board) = board_result {
-        for second in first..64 {
-            if let Ok(board) = board.set(second) {
-                for third in second..64 {
-                    if let Ok(board) = board.set(third) {
-                        println!("{}, {}, {}", first, second, third);
-                        board.output();
-                        break;
+    let board = Board::new();
+    for first in 0..64 {
+        if let Ok(board) = board.set(first) {
+            for second in first..64 {
+                if let Ok(board) = board.set(second) {
+                    for third in second..64 {
+                        if let Ok(board) = board.set(third) {
+                            println!("{}, {}, {}", first, second, third);
+                            board.output();
+                            break;
+                        }
                     }
+                    break;
                 }
-                break;
             }
+            break;
         }
     }
 }
