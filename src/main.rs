9c2642837d8ce2a5a04d3608fa8c28a1f46ed2bc
mark <mark.slater@mail.com>
2023-10-27 19:50:57 +0100

    Dabble with destructuring.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     let mut board = Board::new();
     let mut place = 0;
     for queen in 0..6 {
-        if let Ok(t) = board.place_next(place){
-            board = t.0;
-            place = t.1;
+        if let Ok((b, p)) = board.place_next(place){
+            board = b;
+            place = p;
             println!("{} at {}", queen, place)
         } else {
             println!("Fail :(")
