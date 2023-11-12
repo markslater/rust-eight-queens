3fc02a6a947025863f39558ed048467ce3452940
Mark Slater <mark.slater@mail.com>
2023-11-12 16:26:42 +0000

    Move logic out of main.rs into lib.rs.

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use crate::board::Board;
+
+mod board;
+
+pub fn place_queens() {
+    attempt_to_place_queen_at(&Board::new(), 0, 0).unwrap();
+}
+
+fn attempt_to_place_queen_at(board: &Board, start_at: usize, queen_number: i32) -> Result<(), ()> {
+    if queen_number == 8 {
+        board.output();
+        Ok(())
+    } else {
+        let mut place = start_at;
+        loop {
+            if place == 64 {
+                break Err(())
+            } else {
+                let (candidate_board, candidate_place) = board.place_next(place)?;
+                if attempt_to_place_queen_at(&candidate_board, candidate_place, queen_number + 1).is_ok() {
+                    println!("{}: {}, {}", place, place / 8, place % 8);
+                    break Ok(())
+                }
+            }
+            place = place + 1;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use board::Board;
-
-mod board;
-
 fn main() {
-    let mut board = Board::new();
-    let mut place = 0;
-    for queen in 0..6 {
-        if let Ok((b, p)) = board.place_next(place){
-            board = b;
-            place = p;
-            println!("{} at {}", queen, place)
-        } else {
-            println!("Fail :(")
-        }
-    }
-    board.output();
+    // let mut board = Board::new().set(0).unwrap();
+    // board = board.set(12).unwrap();
+    // board = board.set(23).unwrap();
+    // board = board.set(26).unwrap();
+    // board = board.set(29).unwrap();
+
+    eight_queens::place_queens();
+    // let mut board = Board::new();
+    // let mut place = 0;
+    // for queen in 0..6 {
+    //     if let Ok((b, p)) = board.place_next(place){
+    //         board = b;
+    //         place = p;
+    //         println!("{} at {}", queen, place)
+    //     } else {
+    //         println!("Fail :(")
+    //     }
+    // }
+    // board.output();
 }
