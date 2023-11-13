99c59cd8817fe790dfcc2c2180dc4ffe655ead56
Mark Slater <mark.slater@mail.com>
2023-11-13 19:26:39 +0000

    Print results as coordinates.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ pub fn place_queens() {
     let results: Vec<Vec<usize>> = Board::new().place_queens(0, 8).collect();
     println!("Found {} results", results.len());
     for result in results {
-        println!("{:?}", result)
+        println!("[{}]", result
+            .iter()
+            .map(|square| format!("({}, {})", square % 8, square / 8))
+            .collect::<Vec<String>>()
+            .join(", "));
     }
 }
