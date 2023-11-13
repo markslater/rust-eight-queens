c37000b6b7ee7c6d5d37eba828d0d150ed1ca52c
Mark Slater <mark.slater@mail.com>
2023-11-13 16:57:03 +0000

    Edge towards iterator of valid boards.

--- src/board.rs
+++ src/board.rs
@@ -9,6 +9,13 @@ impl Board {
         }
     }
 
+    fn place_queens(&self, start_at: usize, number_to_place: u32) -> impl Iterator<Item = usize> {
+        match number_to_place {
+            0 => vec![1usize; 0],
+            _ => (start_at..64).filter(|square| !self.squares[*square]).collect::<Vec<usize>>(),
+        }.into_iter()
+    }
+
     fn set(&self, square: usize) -> Result<Board, ()> { // TODO should we use a smaller argument?  Does it matter?
         if self.squares[square] { // TODO should we improve error when index is out of bounds?
             Err(())
@@ -68,6 +75,18 @@ impl Board {
 #[cfg(test)]
 mod tests {
     use crate::board::Board;
+
+    #[test]
+    fn can_place_zero_queens() {
+        let places = Board::new().place_queens(0, 0);
+        assert_eq!(places.collect::<Vec<usize>>(), vec![]);
+    }
+
+    #[test]
+    fn can_place_one_queen() {
+        let places = Board::new().place_queens(0, 1);
+        assert_eq!(places.collect::<Vec<usize>>(), (0..64).collect::<Vec<usize>>());
+    }
 
     #[test]
     fn set_rejects_placement_on_same_square_twice() {
