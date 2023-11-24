f4e0c114ce00de27230277a506f4791ee3645839
Mark Slater <mark.slater@mail.com>
2023-11-24 11:33:33 +0000

    Easier to read if result count is output last.

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,12 @@ mod board;
 
 pub fn place_queens() {
     let results: Vec<Vec<usize>> = Board::new().place_queens(0, 8).collect();
-    println!("Found {} results", results.len());
+    let result_count = results.len();
     for result in results {
         println!("[{}]", result.iter()
             .map(|square| format!("({}, {})", square % 8, square / 8))
             .collect::<Vec<String>>()
             .join(", "));
     }
+    println!("{} results", result_count);
 }
